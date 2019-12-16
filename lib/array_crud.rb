4aa149d4c2c45735796aacb9e2d7c6d1f6c13c08
Eryk Rachwal <erykrachwal@yahoo.com>
2019-12-16 06:08:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  create_an_array = ["a", "b", "c", "d"]
+  create_an_array = ["a", "am", "c", "d"]
   create_an_array[1] 
 end
 
